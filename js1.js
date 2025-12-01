b10791ad4b6d95edef6a63522dc34ad79260e93a
BR_Jr. <userjosine@gmail.com>
2025-12-01 04:16:00 -0400

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,44 @@
+var z1='ENTER'
+var z2= 'FORMULARIO'
+var total=z1+' '+z2
+document.getElementById("z1").innerHTML=(total)
+
+function foco(){
+    document.getElementById("inputtxt").value="";}
+
+    function pi1(){
+       let p= document.getElementById("pi1")
+ 
+    p.append('completado')
+    }
+     function pi2(){
+       let p= document.getElementById("pi2")
+ 
+    p.append('completado')
+}
+     function pi3(){
+       let p= document.getElementById("pi3")
+ 
+    p.append('completado')}
+
+    function zzz(){
+   document.body.style.backgroundImage = 'url("w (2).jpg")';
+}
+ function zz(){
+   document.body.style.backgroundImage = 'url("w (1).jpg")';
+}
+function z(){
+   document.body.style.backgroundImage = 'url("w (3).jpg")';
+}
+function ld(){
+   document.body.style.backgroundColor="white"
+}
+function ente(){
+let div=document.getElementById("ente")
+    div.style.backgroundColor="rgba(122, 0, 126, 1)"
+}
+function ent(){
+let div=document.getElementById("ente")
+    div.style.backgroundColor="rgba(160, 72, 72, 0.48)"
+}
+
